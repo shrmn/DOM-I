b987ac65a870b00b86e431b407b4727e8f4cb588
Dan Sherman <dvsherman@protonmail.com>
2019-01-28 16:25:14 -0700

    created selectors for remaining content and updated all content

--- js/index.js
+++ js/index.js
@@ -42,19 +42,53 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 let navLinks = document.querySelectorAll('a');
-navLinks[0].textContent = siteContent["nav"]["nav-item-1"];
-navLinks[1].textContent = siteContent["nav"]["nav-item-2"];
-navLinks[2].textContent = siteContent["nav"]["nav-item-3"];
-navLinks[3].textContent = siteContent["nav"]["nav-item-4"];
-navLinks[4].textContent = siteContent["nav"]["nav-item-5"];
-navLinks[5].textContent = siteContent["nav"]["nav-item-6"];
+// navLinks[0].textContent = siteContent["nav"]["nav-item-1"];
+// navLinks[1].textContent = siteContent["nav"]["nav-item-2"];
+// navLinks[2].textContent = siteContent["nav"]["nav-item-3"];
+// navLinks[3].textContent = siteContent["nav"]["nav-item-4"];
+// navLinks[4].textContent = siteContent["nav"]["nav-item-5"];
+// navLinks[5].textContent = siteContent["nav"]["nav-item-6"];
 
-let ctaH1 = document.getElementsByTagName('h1');
-ctaH1[0].textContent = siteContent["cta"]["h1"];
+let navLinkKeys = Object.keys(siteContent.nav).filter(keys => {
+  if (keys.includes("nav")) {
+    return keys;
+  }
+});
 
-let ctaBtn = document.getElementsByTagName('button');
-ctaBtn[0].textContent = siteContent["cta"]["button"];
+navLinks.forEach((link, index) => {
+  link.textContent = siteContent["nav"][navLinkKeys[index]];
+}) 
+
+let ctaH1 = document.querySelector('.cta h1');
+ctaH1.textContent = siteContent["cta"]["h1"];
+
+let ctaBtn = document.querySelector('.cta button');
+ctaBtn.textContent = siteContent["cta"]["button"];
 
 let ctaImg = document.getElementById("cta-img");
 ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
-console.log(ctaImg);
+
+let mainH4 = document.querySelectorAll('.main-content h4');
+mainH4[0].textContent = siteContent["main-content"]["features-h4"]
+mainH4[1].textContent = siteContent["main-content"]["about-h4"]
+mainH4[2].textContent = siteContent["main-content"]["services-h4"]
+mainH4[3].textContent = siteContent["main-content"]["product-h4"]
+mainH4[4].textContent = siteContent["main-content"]["vision-h4"]
+
+let mainP = document.querySelectorAll('.main-content p');
+mainP[0].textContent = siteContent["main-content"]["features-content"]
+mainP[1].textContent = siteContent["main-content"]["about-content"]
+mainP[2].textContent = siteContent["main-content"]["services-content"]
+mainP[3].textContent = siteContent["main-content"]["product-content"]
+mainP[4].textContent = siteContent["main-content"]["vision-content"]
+
+let middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+let contacth4 = document.querySelector('.contact h4');
+contacth4.textContent = siteContent["contact"]["contact-h4"];
+
+let contactP = document.querySelectorAll('.contact p');
+contactP[0].textContent = siteContent["contact"]["address"];
+contactP[1].textContent = siteContent["contact"]["phone"];
+contactP[2].textContent = siteContent["contact"]["email"];
